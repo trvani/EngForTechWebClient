7c6f01557d2851d44bc580904d7bcba219726ea2
safakkaya <safak.kaya@outlook.com>
2023-11-19 13:44:37 +0300

    WIP Modules and structural design
    Product and structural design is ignited. Admin and ui modules are added to project

---
+++ src/app/admin/layout/layout.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './layout.component';
+import { ComponentsModule } from "./components/components.module";
+
+
+
+@NgModule({
+    declarations: [
+        LayoutComponent
+    ],
+    exports: [
+        LayoutComponent
+    ],
+    imports: [
+        CommonModule,
+        ComponentsModule
+    ]
+})
+export class LayoutModule { }
